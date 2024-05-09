494a37ac21a910d2b2b2dc6752ba72f4725b8ee3
imix108 <136431946+imix108@users.noreply.github.com>
2024-05-09 17:39:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.NODE_ENV === 'production' ? 'landing-page_01' : '/';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: base,
   plugins: [react()],
 })
